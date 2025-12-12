51d6d419a50fe4936b5aa183e3089a51974c06c2
denyskozak <denys.kozak.self@gmail.com>
2025-12-12 15:57:43 +0000

    chore: changes

--- backend/src/http/cors.ts
+++ backend/src/http/cors.ts
@@ -6,7 +6,7 @@ const ALLOWED_ORIGINS = new Set<string>([
     'https://talegrafrontendadmin.gastroand.me',
     'http://localhost:5174',
     'https://172.20.10.7:5173',
-    'https://172.20.10.2:5173/'
+    'https://172.20.10.2:5173'
 ]);
 
 const ALLOWED_METHODS = 'GET,POST,PUT,PATCH,DELETE,OPTIONS';
